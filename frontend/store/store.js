e18e9e5bbd80ae32506e4aca2cbc72039ccd18b6
t_blarel <tblarel@gmail.com>
2019-07-10 17:07:46 -0700

    Remove logger for production.

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -14,7 +14,7 @@ const configureStore = (preloadedState = {}) => {
     return createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(logger, ...middleware)
+        applyMiddleware(...middleware)
     );
 };
 
