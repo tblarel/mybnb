9cd8629049a9a00275b2fb5c3f5aa2af64a63072
t_blarel <tblarel@gmail.com>
2019-07-27 09:55:06 -0700

    Install react router and use it to display star's rating on posted reviews.

--- frontend/components/spots/spot_reviews.jsx
+++ frontend/components/spots/spot_reviews.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import moment from 'moment';
-
+import Rater from 'react-rater'
+import 'react-rater/lib/react-rater.css'
 
 class SpotReviews extends React.Component {
 
@@ -22,7 +23,9 @@ class SpotReviews extends React.Component {
                                 <h3 className="review-author">{review.guest_fname} {review.guest_lname}</h3> 
                                 <h4 className="review-date">{new moment(review.date).format('LL')}</h4>
                             </div>
-                            <h4 className="review-rating">{review.rating} Stars </h4>
+                            <div className="review-row">
+                                <Rater total={5} rating={review.rating} interactive={false}/>
+                            </div>
                             <h4 className="review-body">{review.body} </h4>
                         </div>
                     </div>
